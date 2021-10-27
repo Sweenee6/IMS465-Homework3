d25551b9210bb6d2f882f248fbe10d773a866d65
Sweenee6 <73003974+Sweenee6@users.noreply.github.com>
2021-10-27 16:56:01 -0400

    Player Input and Movement
    Input system and player movement via Rigidbody.MovePosition

---
+++ IMS465_Physics/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private PlayerInput playerInput;
+    //[SerializeField] private MeshRenderer meshRenderer;
+    private Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + (direction * speed * Time.deltaTime));
+    }
+
+    public void OnMove(InputValue val)
+    {
+        var dir = val.Get<Vector2>();
+        direction = new Vector3(dir.x,dir.y,0);
+    }
+
+}
